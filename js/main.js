38d73e43942dfbcc5315d001fc8cff2a65f1ab31
Mark N Broadhead <MarkNBroadhead@gmail.com>
2018-04-13 23:00:28 -0600

    Add blocks, initializations, and render loop

---
+++ js/main.js
@@ -0,0 +1,130 @@
+var canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('myCanvas'))
+var ctx = canvas.getContext('2d')
+canvas.height = 700
+canvas.width = 1000
+
+class Ball {
+    constructor(x, y, speed, color, radius, isStuckToPaddle) {
+        this.x = x
+        this.y = y
+        this.speed = speed
+        this.color = color
+        this.radius = radius
+        this.isStuckToPaddle = isStuckToPaddle
+    }
+    draw() {
+        ctx.beginPath()
+        ctx.fillStyle = this.color
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
+        ctx.fill()
+    }
+}
+
+class RainbowBall {
+    constructor(x, y, speed, insideColor, outsideColor, radius, isStuckToPaddle) {
+        this.x = x
+        this.y = y
+        this.speed = speed
+        this.insideColor = insideColor
+        this.outsideColor = outsideColor
+        this.radius = radius
+        this.isStuckToPaddle = isStuckToPaddle
+    }
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
+        this.gradient = ctx.createRadialGradient(this.x, this.y, this.radius, this.x, this.y, 1)
+        this.gradient.addColorStop(0, this.outsideColor)
+        this.gradient.addColorStop(1, this.insideColor)
+        ctx.fillStyle = this.gradient
+        ctx.fill()
+    }
+}
+
+class Paddle {
+    constructor(x, y, maxSpeed, color, width, height, isSticky) {
+        this.x = x
+        this.y = y
+        this.maxSpeed = maxSpeed
+        this.color = color
+        this.width = width
+        this.height = height
+        this.isSticky = isSticky
+    }
+    draw() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+    releaseBall() {
+
+    }
+}
+
+class Block {
+    constructor(x, y, width, height, color, health) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.color = color
+        this.health = health
+    }
+    draw() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+/* Initialize Balls */
+var firstBall = new Ball(200, 300, 1, 'rgb(40, 65, 255)', 70, false)
+var rainbow = new RainbowBall(870, 320, 1, 'rgb(102, 0, 255)', 'rgb(255, 0, 238)', 100, false)
+var balls = []
+balls.push(firstBall, rainbow)
+
+/* Initialize Blocks */
+var blockWidth = 101
+var blockHeight = 40
+var numBlockColumns = Math.floor(canvas.width / blockWidth)
+var xPadding = (canvas.width % blockWidth) / 2
+var numBlockRows = 5
+var blocks = initializeBlocks(numBlockColumns, numBlockRows, xPadding, 50, blockWidth, blockHeight)
+
+/* Initialize Paddle */
+var paddleWidth = 200
+var paddleHeight = 40
+var paddle = new Paddle(canvas.width/2-paddleWidth/2, canvas.height-paddleHeight, 5, 'black', paddleWidth, paddleHeight, false)
+
+// var drawableObjects = [paddle, ...blocks, ...balls]
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // for (var obj of drawableObjects) {
+    // obj.draw()
+    // }
+    paddle.draw()
+    for (var block of blocks) {
+        block.draw()
+    }
+    for (var ball of balls) {
+        ball.draw()
+    }
+}
+
+(function () {
+    function main() {
+        window.requestAnimationFrame( main )
+        draw()
+    }
+    main()
+})()
+
+function initializeBlocks(columns, rows, col1X, row1Y, width, height) {
+    let newBlocks = []
+    let blockColors = ['red', 'orange', 'yellow', 'blue', 'green', 'pink', 'orange', 'purple']
+    for (let x = 0; x < columns; x++) {
+        for (let y = 0; y < rows; y++) {
+            var nextColor = blockColors[Math.floor(Math.random() * blockColors.length)]
+            newBlocks.push(new Block(col1X + x * width, row1Y + y * height, width, height, nextColor, 1))
+        }
+    }
+    return newBlocks
+}
